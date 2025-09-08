30c492e5269f99b5f58f5032c198fe31018c9263
Oh-Kang94 <okh19941994@naver.com>
2025-09-08 15:40:50 +0900

    소켓 클라이언트 생성 및 닫기 구현

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -11,4 +13,48 @@ type Client struct {
 	send   chan *Message   // 메시지 전송용 채널
 	roomId string          // 현재 접속한 채팅방
 	user   *User
+}
+
+const messageBufferSize = 256
+
+func newCleint(conn *websocket.Conn, roomId string, u *User) {
+	c := &Client{
+		conn:   conn,
+		send:   make(chan *Message, messageBufferSize),
+		roomId: roomId,
+		user:   u,
+	}
+
+	// 클라이언트 목록에 새로운 클라이언트 추가
+	clients = append(clients, c)
+
+	// 메시지 수신/전송 대기
+	go c.readLoop()
+	go c.writeLoop()
+}
+
+func (c *Client) Close() {
+	for i, client := range clients {
+		// 딱 해당하는 것만 빼기
+		if client == c {
+			clients = append(clients[:i], clients[i+1:]...)
+		}
+		break
+	}
+	// Send 채널 닫음
+	close(c.send)
+
+	// 웹소켓 종료
+	c.conn.Close()
+
+	log.Printf("close conn. addr: %s", c.conn.RemoteAddr())
+
+}
+
+func (c *Client) readLoop() {
+	panic("unimplemented")
+}
+
+func (c *Client) writeLoop() {
+	panic("unimplemented")
 }
